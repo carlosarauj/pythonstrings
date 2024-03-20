bc12d3184418d7c2fd1911ebed05e29349d06b92
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-03-20 18:04:47 -0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,9 @@
+strin = "carlos araujo "
+
+print(strin.upper())
+print(strin.lower())
+print(strin.capitalize())
+print(strin.isupper())
+print(strin.islower())
+print(strin.strip())
+print(strin.replace("araujo", "ferreira"))
